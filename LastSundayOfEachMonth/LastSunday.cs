c7b8adea613a48600ced4b99a11ef4f2fa1eeb79
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-06-12 12:56:37 +0200

    completed kata

---
+++ LastSundayOfEachMonth/LastSunday.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastSundayOfEachMonth
+{
+    public class LastSunday
+    {
+        public List<string> GetLastSundayInMonth(int year)
+        {
+            DateTime date;
+            List<string> listOfSundays = new List<string>();
+
+            for (int month = 1; month <= 12; month++)
+            {
+                date = new DateTime(year, month, DateTime.DaysInMonth(year, month));
+                while (date.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    date = date.AddDays(-1);
+
+                }
+                listOfSundays.Add(date.ToString("yyyy-MM-dd"));
+                
+            }
+            return listOfSundays;
+        }
+    }
+}
+
+
---
+++ LastSundayOfEachMonthTests/LastSundayOfEachMonthTests.cs
@@ -0,0 +1,131 @@
+﻿using LastSundayOfEachMonth;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastSundayOfEachMonthTests
+{
+    [TestFixture]
+    public class LastSundayOfEachMonthTests
+    {
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2013_ShouldReturnLastSundayOfMonth1()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2013-01-27";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2013);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.First(), expected);
+        }
+
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2013_ShouldReturnLastSundayOfMonth2()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2013-02-24";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2013);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.ElementAt(1), expected);
+        }
+
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2013_ShouldReturnLastSundayOfMonth6()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2013-06-30";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2013);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.ElementAt(5), expected);
+        }
+        
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2013_ShouldReturnLastSundayOfMonth11()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2013-11-24";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2013);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.ElementAt(10), expected);
+        }
+
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2013_ShouldReturnLastSundayOfMonth12()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2013-12-29";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2013);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.Last(), expected);
+        }
+
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2018_ShouldReturnLastSundayOfMonth1()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2018-01-28";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2018);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.First(), expected);
+        }
+
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2018_ShouldReturnLastSundayOfMonth6()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2018-06-24";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2018);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.ElementAt(5), expected);
+        }
+
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2020_ShouldReturnLastSundayOfMonth2()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2020-02-23";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2020);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.ElementAt(1), expected);
+        }
+
+        [Test]
+        public void GetLastSundayInMonth_GivenTheYear2032_ShouldReturnLastSundayOfMonth2()
+        {
+            //---------------Arrange-------------------
+            var last = new LastSunday();
+            var expected = "2032-02-29";
+            //---------------Act----------------------
+            var actual = last.GetLastSundayInMonth(2032);
+
+            //---------------Assert----------------------
+            Assert.AreEqual(actual.ElementAt(1), expected);
+        }
+    }
+}
